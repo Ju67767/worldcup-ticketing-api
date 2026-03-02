8bb25c61311e1b94f2f59b26ab11faa46a71bf5a
Justine Simon-Perard <justinesimon-perard@MacBook-Pro-de-Justine.local>
2026-03-02 15:11:26 +0100

    étape fichier .env.example fait

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,30 @@ import { Hono } from 'hono'
 export const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.json(
+    {
+      success : true,
+      message : "World Cup Ticketing API"
+
+    },
+    200
+  );
+});
+
+app.get('/health', (c) => {
+  return c.json(
+    {
+      success : true,
+      message : "World Cup Ticketing API",
+      uptime : 0,
+      environment : "dev"
+
+      
+      
+
+    },
+    200
+  );
+});
 
 
